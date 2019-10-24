8d103459575b8d6e27bd04b448b1ce624465e69f
WANG xin <wangx@informatique.univ-paris-diderot.fr>
2019-10-24 12:03:47 +0200

    sac lettre

--- src/scrabblos/Client.java
+++ src/scrabblos/Client.java
@@ -2,12 +2,16 @@ package scrabblos;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import org.json.JSONObject;
 
 import java.security.MessageDigest;
@@ -21,79 +25,100 @@ import net.i2p.crypto.eddsa.spec.EdDSAParameterSpec;
 import net.i2p.crypto.eddsa.spec.EdDSAPublicKeySpec;
 
 public class Client {
-	
-	 private static Socket socket;
-	 
-	 private String getPublicKey() {
-		 try {
+
+	private static Socket socket;
+
+	private String getPublicKey() {
+		try {
 			Signature sgr = new EdDSAEngine(MessageDigest.getInstance("SHA-512"));
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 EdDSAParameterSpec spec = EdDSANamedCurveTable.getByName("ed25519-sha-512");
-		 
-		 return "";
-	 }
-	 
-	    public static void main(String args[])
-	    {
-	        try
-	        {
-	            String host = "localhost";
-	            int port = 12345;
-	            InetAddress address = InetAddress.getByName(host);
-	           // System.err.print(address);
-	            socket = new Socket(address, port);
-	 
-	            //Send the message to the server
-	            OutputStream os = socket.getOutputStream();
-	            OutputStreamWriter osw = new OutputStreamWriter(os);
-	            BufferedWriter bw = new BufferedWriter(osw);
-	            
-	            JSONObject json = new JSONObject();
-	            int k = 2*json.toString().toCharArray().length;
-	            json.put("register","ed25519");
-	            
-	            char [] strs = {'\000','\000','\000','\000','\000','\000','\000','\004'};
-	 
-	            String mes = "";
-	            for(int i=0;i<strs.length;i++) {
-	            	mes += strs[i];
-	            }
-	            mes +=json.toString();
-	            bw.write(mes);
-	            
-	            
-	            bw.flush();
-	            System.out.println("Message sent to the server : "+json.toString());
-	            
+		EdDSAParameterSpec spec = EdDSANamedCurveTable.getByName("ed25519-sha-512");
+
+		return "";
+	}
+
+	public static void main(String args[]) {
+		try {
+			String host = "localhost";
+			int port = 12346;
+			InetAddress address = InetAddress.getByName(host);
+			// System.err.print(address);
+			socket = new Socket(address, port, true);
+
+			// Send the message to the server
+			OutputStream os = socket.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(osw);
+
+			JSONObject json = new JSONObject();
+			int k = 2 * json.toString().toCharArray().length;
+			json.put("register", "b7b597e0d64accdb6d8271328c75ad301c29829619f4865d31cc0c550046a08f");
+			byte [] a =intToBigEndian(json.toString().length());
+			String mes = "";
+			
+			for(int i = a.length-1 ;i>=0 ;i--) {
+				mes +=Integer.toString(a[i],8);
+			}
+			
+			//mes = "\000\000\000\000\000\000\000\117"+json.toString();
+			mes += json.toString();
+			bw.write(mes);
+			//System.out.println("size" + json.toString().toCharArray().length);
+
+			bw.flush();
+			System.out.println("Message sent to the server : " + mes);
+
 //	            JSONObject getlettres = new JSONObject();
 //	            json.put("get_full_letterpool",null);
-	 
-	            //Get the return message from the server
-	            InputStream is = socket.getInputStream();
-	            InputStreamReader isr = new InputStreamReader(is);
-	            BufferedReader br = new BufferedReader(isr);
-	            String message = br.readLine();
-	            System.out.println("Message received from the server : " +message);
-	        }
-	        catch (Exception exception)
-	        {
-	            exception.printStackTrace();
-	        }
-	        finally
-	        {
-	            //Closing the socket
-	            try
-	            {
-	                socket.close();
-	            }
-	            catch(Exception e)
-	            {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
 
+			// Get the return message from the server
+			System.out.println("port : " + socket.getReceiveBufferSize());
+
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String message = "";
+			
+
+			System.out.println("here : ");
+			message = br.readLine();
+			System.out.println("Message received from the server : ");
+			
+			//br.close();
+
+			System.out.println("leave" + message);
+		} catch (IOException exception) {
+			exception.printStackTrace();
+			System.out.println("exception : " + exception.getMessage());
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			System.out.println("exception : " + exception.getMessage());
+		} finally {
+			// Closing the socket
+			try {
+				socket.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("e : " + e.getMessage());
+			}
+		}
+	}
+	
+	public static String byteToHex(byte b) {
+	    int i = b & 0xFF;
+	    return Integer.toHexString(i);
+	  }
+	
+	private static byte[] intToBigEndian(int numero) {
+		ByteBuffer bb = ByteBuffer.allocate(8);
+		bb.rewind();
+		bb.order(ByteOrder.LITTLE_ENDIAN);
+		bb.putInt((int) numero);
+		System.out.println(bb);
+		return bb.array();
+		
+	}
 }

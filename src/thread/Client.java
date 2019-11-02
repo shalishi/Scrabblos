da839a6365db791ef56194ce403b812979689d30
WANG xin <wangx@informatique.univ-paris-diderot.fr>
2019-11-02 14:50:55 +0100

    client get word

--- src/thread/Client.java
+++ src/thread/Client.java
@@ -9,16 +9,11 @@ import java.security.NoSuchProviderException;
 import java.security.SignatureException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import net.i2p.crypto.eddsa.EdDSAPublicKey;
 import scrabblos.ED25519;
 import scrabblos.Letter;
+import scrabblos.LetterPool;
 import scrabblos.Utils;
 import scrabblos.Word;
 
@@ -26,7 +21,7 @@ public class Client implements Runnable {
 	private static String pk;
 	private static KeyPair kp;
 	private ArrayList<String> LetterBag = new ArrayList<String>();
-	private String hash = ""+Math.random()*100;
+	private String hash = "" + Math.random() * 100;
 	private char[] bags = { 'b', 'd', 'u', 'q', 's', 'y', 'o', 'r', 'r', 'p', 'm', 'e', 'p', 'y', 's', 'l', 't', 'h',
 			'u', 'i', 'n', 'p', 'w', 't', 'w', 'a', 'e', 's', 'r', 'c', 'y', 'c', 'u', 'j', 'x', 't', 'i', 'o', 'k',
 			'k', 'c', 'c', 'l', 'w', 'y', 'c', 'w', 'o', 'y', 'x', 'g', 'c', 'u', 'y', 'g', 's', 's', 'c', 'q', 'q',
@@ -46,10 +41,9 @@ public class Client implements Runnable {
 			creatKey();
 			// LetterBag = new ArrayList<String>();
 
-			for(char c:bags) {
-				LetterBag.add(c+"");
+			for (char c : bags) {
+				LetterBag.add(c + "");
 			}
-			
 
 			updateLetterPool();
 			/*
@@ -64,31 +58,63 @@ public class Client implements Runnable {
 
 	private String getWordSiganture() {
 		ArrayList<Word> wordPool = readWordPool();
-		if(wordPool.size()>0) {
+		if (wordPool.size() > 0) {
 			int maxSize = 0;
-			int maxSizeIndex=0;
-			for(Word w : wordPool) {
-				if(w.getWord().size()>maxSize) {
-					maxSize = w.getWord().size();
+			int maxSizeIndex = 0;
+			for (Word w : wordPool) {
+				String str = w.getHash();
+				int size = getPreSize(str);
+				size += w.getWord().size();
+				if (size > maxSize) {
+					maxSize = size;
 					maxSizeIndex = wordPool.indexOf(w);
 				}
 			}
-			return wordPool.get(maxSizeIndex).getSignature();		
+			return wordPool.get(maxSizeIndex).getSignature();
 		}
 		return "";
 	}
-	
+
+	private int getPreSize(String hash) {
+		ArrayList<Word> wordPool = readWordPool();
+		MessageDigest digest;
+		try {
+			for (Word w : wordPool) {
+				digest = MessageDigest.getInstance("SHA-256");
+				String sighash = Utils.bytesToHex(digest.digest((w.getSignature()).getBytes()));
+				if (sighash == hash) {
+					int size = w.getWord().size();
+					if (w.getPeriod() > 0) {
+						hash = w.getHash();
+						System.out.println("size  = " +size);
+						return size + getPreSize(hash);
+					}
+					if (w.getPeriod() == 0) {
+						System.out.println("size  = " +size);
+						return size;
+					}
+				}
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+
+	}
+
 	private ArrayList<Word> readWordPool() {
-		System.out.println("Thread Client " + Thread.currentThread().getId() + " is reading word pool*********************************************");
+		System.out.println("Thread Client " + Thread.currentThread().getId()
+				+ " is reading word pool*********************************************");
 		synchronized (MotorA.getMotorA()) {
 
 			MotorA motor = MotorA.getMotorA();
-			System.out
-					.println("Thread Client " + Thread.currentThread().getId() + " is finished of reading word pool**********************************");
+			System.out.println("Thread Client " + Thread.currentThread().getId()
+					+ " is finished of reading word pool**********************************");
 			return motor.getWord_pool().getWords();
 		}
 	}
-	
+
 	private void creatKey() {
 		// CREATION DE LA CLE PUBLIQUE
 		ED25519 ed;
@@ -111,7 +137,7 @@ public class Client implements Runnable {
 			String letter = LetterBag.get(0);
 			String signature = Utils.bytesToHex(Utils.signature2(letter, digest.digest((hash).getBytes()), 0, kp));
 			String head = Utils.bytesToHex(digest.digest((hash).getBytes()));
-			long period = 0;
+			int period = getLetterPool().getCurrent_period();
 			return new Letter(letter, period, head, pk, signature);
 		} catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException | IOException e) {
 			// TODO Auto-generated catch block
@@ -121,15 +147,22 @@ public class Client implements Runnable {
 	}
 
 	protected void updateLetterPool() {
-		
-			System.out.println("update letter pool");
-			Letter l = chooseLetter(LetterBag, getWordSiganture());
-			//String j = "{ \"inject_letter\": { \"letter\":\"a\", \"period\":0, \"head\":\"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\", \"author\":\"b7b597e0d64accdb6d8271328c75ad301c29829619f4865d31cc0c550046a08f\", \"signature\":\"8b6547447108e11c0092c95e460d70f367bc137d5f89c626642e1e5f2ce\" }}";
-			synchronized (MotorA.getMotorA()) {
-				MotorA motor = MotorA.getMotorA();
-				motor.addLetter(l);
-				motor.showLetterPool();
-			}
+
+		System.out.println("update letter pool");
+		Letter l = chooseLetter(LetterBag, getWordSiganture());
+		synchronized (MotorA.getMotorA()) {
+			MotorA motor = MotorA.getMotorA();
+			motor.addLetter(l);
+			motor.showLetterPool();
+		}
+	}
+
+	protected LetterPool getLetterPool() {
+
+		synchronized (MotorA.getMotorA()) {
+			MotorA motor = MotorA.getMotorA();
+			return motor.getLetter_pool();
+		}
 
 	}
 
